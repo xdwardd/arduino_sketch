bd9efb397272f133c5a9ba5d467e7ef2a5fbd206
Edward Abrera Catapan <ecatapan@wellmade-motors.com>
2021-09-24 14:51:34 +0800

    first commit

---
+++ program/program/program.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(115200);
+  Serial.println("setup()");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  Serial.println("loop()");
+  delay(2000);
+}
